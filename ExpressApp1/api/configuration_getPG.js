c04e7a93205f889f01e4de8627bea08997f47544
taketanaka <takenori_tanaka@niigata-sl.com>
2015-03-16 17:38:04 +0900

    2015_03_16_01

---
+++ ExpressApp1/api/configuration_getPG.js
@@ -0,0 +1,34 @@
+﻿// 動作設定データの取得
+exports.configuration_get = function (req, res) {
+	if ((req.params.id != undefined) && (req.params.id != '')) {
+		var sql = 'SELECT ' 
+			+ 'id,' 
+			+ 'drc_name,' 
+			+ 'drc_address1,' 
+			+ 'drc_address2,' 
+			+ 'drc_telno,' 
+			+ 'drc_faxno,' 
+			+ 'consumption_tax,' 
+			+ "to_char(created,'YYYY/MM/DD HH24:MI:SS') AS created," 
+			+ 'created_id,' 
+			+ "to_char(updated,'YYYY/MM/DD HH24:MI:SS') AS updated," 
+			+ 'updated_id' 
+			+ ' FROM drc_sch.configuration WHERE id = $1'; 
+		// SQL実行
+		pg.connect(connectionString, function (err, connection) {
+			// データを取得するためのクエリーを実行する
+			connection.query(sql, [req.params.id], function (err, results) {
+				if (err) {
+					console.log(err);
+				} else {
+					var config = [];
+					for (var i in results.rows) {
+						config = results.rows[i];
+					}
+					connection.end();
+					res.send(config);
+				}
+			});
+		});
+	}
+};
