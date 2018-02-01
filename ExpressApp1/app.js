60b4c6e4ed3641899841cdac5b4ea0d5904047fb
labradorkuro <jazzsaxplayer02@gmail.com>
2018-02-01 18:04:59 +0900

    port no 8080

--- ExpressApp1/app.js
+++ ExpressApp1/app.js
@@ -96,7 +96,7 @@ var path = require('path');
 drc_version = ' Ver.1.0.3.6';
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
