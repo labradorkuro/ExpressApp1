8a44ef8126b3e0728897de3b2c74987b1cdd89d6
Takenori Tanaka <takenori_tanaka@niigata-sl.com>
2020-01-06 12:22:04 +0900

    portを8080に変更

--- ExpressApp1/app.js
+++ ExpressApp1/app.js
@@ -103,7 +103,7 @@ var path = require('path');
 drc_version = ' Ver.1.1.5';
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
